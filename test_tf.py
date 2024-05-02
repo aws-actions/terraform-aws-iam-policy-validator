7a90d21844e420839a9dfce38d9a641a6d314b91
Li-Juen Huang <hlijuen@amazon.com>
2024-05-02 13:35:21 -0700

    Unittest of methods defined in main.py

--- test_tf.py
+++ test_tf.py
@@ -1,127 +1,115 @@
 import os
 import subprocess
 import unittest
+import pytest
+from parameterized import parameterized
 from main import get_policy_check_type, get_required_inputs, get_optional_inputs, get_sub_command
 from main import get_treat_findings_as_non_blocking_flag, build_command, execute_command, set_output
 
-CLI_POLICY_VALIDATOR = "tf-policy-validator"
+from main import CLI_POLICY_VALIDATOR, POLICY_CHECK_TYPE, VALIDATE_POLICY, CHECK_NO_NEW_ACCESS, CHECK_ACCESS_NOT_GRANTED
+from main import COMMON_REQUIRED_INPUTS, TREAT_FINDINGS_AS_NON_BLOCKING
 
-POLICY_CHECK_TYPE = "INPUT_POLICY-CHECK-TYPE"
+from unittest.mock import patch
 
-NOT_VALIDATE_POLICY = "NOT_VALIDATE_POLICY"
-VALIDATE_POLICY = "VALIDATE_POLICY"
-CHECK_NO_NEW_ACCESS = "CHECK_NO_NEW_ACCESS"
-CHECK_ACCESS_NOT_GRANTED = "CHECK_ACCESS_NOT_GRANTED"
-
-COMMON_REQUIRED_INPUTS = {"INPUT_TEMPLATE-PATH", "INPUT_REGION"}
-
-TREAT_FINDINGS_AS_NON_BLOCKING = "INPUT_TREAT-FINDINGS-AS-NON-BLOCKING"
-
-# assertRaises(SomeCoolException, mymod.myfunc)
 class TfpvTest(unittest.TestCase):
-    # test_get_type_case1: failure expected because required os.environ[] are not set")
+    # case 1: test_get_type_ENVIRON_NOT_SET: failure expected because required os.environ[] are not set
     @unittest.expectedFailure
-    def test_get_type_case1(self):
+    def test_get_type_ENVIRON_NOT_SET(self):
         assertRaises(ValueError, get_policy_check_type)
 
-    # test_get_type_case2: failure expected because os.environ[] is set to an invalid value")
+    # case 2: test_get_type_INVALID_POLICY_CHECK: failure expected because os.environ[] is set to an invalid value
     @unittest.expectedFailure
-    def test_get_type_case2(self):
-        os.environ[POLICY_CHECK_TYPE] = NOT_VALIDATE_POLICY
+    def test_get_type_INVALID_POLICY_CHECK(self):
+        os.environ[POLICY_CHECK_TYPE] = INVALID_POLICY_CHECK
         assertRaises(ValueError, get_policy_check_type)
 
-    def test_get_type_case3(self):
-        os.environ[POLICY_CHECK_TYPE] = VALIDATE_POLICY
+    # case 3, 4, 5: test_get_type_WITH_VALIDATE_POLICY: success with valid POLICY_CHECK_TYPE
+    @parameterized.expand([VALIDATE_POLICY, CHECK_NO_NEW_ACCESS, CHECK_ACCESS_NOT_GRANTED])
+    def test_get_type_WITH_VALID_POLICY_CHECK(self, policy_check_type):
+        os.environ[POLICY_CHECK_TYPE] = policy_check_type
         policy_type = get_policy_check_type()
-        assert policy_type == VALIDATE_POLICY
+        self.assertEqual(policy_type, policy_check_type)
 
-    # test_get_required_input_case4: failure expected because an invalid policy_check_type is provided")
+    # case 6: test_get_required_input_INVALID_POLICY_CHECK: failure expected because an invalid policy_check_type is provided
     @unittest.expectedFailure
-    def test_get_required_input_case4(self):
-        policy_check = "INVALIDATE_POLICY"
-        result = get_required_inputs(policy_check)
-        assert result == {"INPUT_TEMPLATE-PATH",  "INPUT_REGION"}
+    def test_get_required_input_INVALID_POLICY_CHECK(self):
+        policy_check = "INVALID_POLICY_CHECK"
+        assert get_required_inputs(policy_check) is None
 
-    def test_get_required_input_case5(self):
-        policy_check = "VALIDATE_POLICY"
-        result = get_required_inputs(policy_check)
-        assert result == {"INPUT_TEMPLATE-PATH",  "INPUT_REGION"}
+    # case 7, 8, 9: test_get_required_input_WITH_VALIDATE_POLICY: success as a valid POLICY_CHECK_TYPE is provided: VALIDATE_POLICY
+    @parameterized.expand([VALIDATE_POLICY, CHECK_NO_NEW_ACCESS, CHECK_ACCESS_NOT_GRANTED])
+    def test_get_required_input_WITH_VALID_POLICY_CHECK(self, policy_check_type):
+        result = get_required_inputs(policy_check_type)
+        if policy_check_type == VALIDATE_POLICY:
+            self.assertEqual(result, {"INPUT_TEMPLATE-PATH",  "INPUT_REGION"})
+        elif policy_check_type == CHECK_NO_NEW_ACCESS:
+            self.assertEqual(result, {"INPUT_TEMPLATE-PATH",  "INPUT_REGION", "INPUT_REFERENCE-POLICY",  "INPUT_REFERENCE-POLICY-TYPE"})
+        else:
+            self.assertEqual(result, {"INPUT_TEMPLATE-PATH",  "INPUT_REGION", "INPUT_ACTIONS"})
 
-    def test_get_required_input_case6(self):
-        policy_check = "CHECK_NO_NEW_ACCESS"
-        result = get_required_inputs(policy_check)
-        assert result == {"INPUT_TEMPLATE-PATH",  "INPUT_REGION", "INPUT_REFERENCE-POLICY",  "INPUT_REFERENCE-POLICY-TYPE"}
-
-    def test_get_required_input_case7(self):
-        policy_check = "CHECK_ACCESS_NOT_GRANTED"
-        result = get_required_inputs(policy_check)
-        assert result == {"INPUT_TEMPLATE-PATH",  "INPUT_REGION", "INPUT_ACTIONS"}
-
-
-    # test_get_optional_input_case8: failure expected because an invalid policy_check_type is provided")
+    # case 10: test_get_optional_input_INVALID_POLICY_CHECK: failure expected because an invalid policy_check_type is provided
     @unittest.expectedFailure
-    def test_get_optional_input_case8(self):
-        policy_check = "INVALIDATE_POLICY"
+    def test_get_optional_input_INVALID_POLICY_CHECK(self):
+        policy_check = INVALID_POLICY_CHECK
         result = get_optional_inputs(policy_check)
-        assert result == {"INPUT_IGNORE-FINDING",  "INPUT_ALLOW-DYNAMIC-REF-WITHOUT-VERSION",  "INPUT_EXCLUDE-RESOURCE-TYPES"}
+        assertEqual(result, {"INPUT_IGNORE-FINDING",  "INPUT_ALLOW-DYNAMIC-REF-WITHOUT-VERSION",  "INPUT_EXCLUDE-RESOURCE-TYPES"})
 
-    def test_get_optional_input_case9(self):
-        policy_check = "VALIDATE_POLICY"
-        result = get_optional_inputs(policy_check)
-        assert result == {"INPUT_ALLOW-EXTERNAL-PRINCIPALS",  "INPUT_TREAT-FINDING-TYPE-AS-BLOCKING", "INPUT_IGNORE-FINDING", \
-                          "INPUT_ALLOW-DYNAMIC-REF-WITHOUT-VERSION",  "INPUT_EXCLUDE-RESOURCE-TYPES"}
+    # case 11, 12, 13: test_get_optional_input_WITH_VALID_POLICY_CHECK: success as a valid POLICY_CHECK_TYPE is provided
+    @parameterized.expand([VALIDATE_POLICY, CHECK_NO_NEW_ACCESS, CHECK_ACCESS_NOT_GRANTED])
+    def test_get_optional_input_WITH_VALID_POLICY_CHECK(self, policy_check_type):
+        result = get_optional_inputs(policy_check_type)
+        if policy_check_type == VALIDATE_POLICY:
+            self.assertEqual(result, {"INPUT_ALLOW-EXTERNAL-PRINCIPALS",  "INPUT_TREAT-FINDING-TYPE-AS-BLOCKING", "INPUT_IGNORE-FINDING",
+                          "INPUT_ALLOW-DYNAMIC-REF-WITHOUT-VERSION",  "INPUT_EXCLUDE-RESOURCE-TYPES"})
+        elif policy_check_type == CHECK_NO_NEW_ACCESS:
+            self.assertEqual(result, {"INPUT_IGNORE-FINDING",  "INPUT_ALLOW-DYNAMIC-REF-WITHOUT-VERSION",  "INPUT_EXCLUDE-RESOURCE-TYPES"})
+        else:
+            self.assertEqual(result, {"INPUT_IGNORE-FINDING",  "INPUT_ALLOW-DYNAMIC-REF-WITHOUT-VERSION",  "INPUT_EXCLUDE-RESOURCE-TYPES"})
 
-    def test_get_optional_input_case10(self):
-        policy_check = "CHECK_NO_NEW_ACCESS"
-        result = get_optional_inputs(policy_check)
-        assert result == {"INPUT_IGNORE-FINDING",  "INPUT_ALLOW-DYNAMIC-REF-WITHOUT-VERSION",  "INPUT_EXCLUDE-RESOURCE-TYPES"}
-
-    def test_get_optional_input_case11(self):
-        policy_check = "CHECK_NO_NEW_ACCESS"
-        policy_check = "CHECK_ACCESS_NOT_GRANTED"
-        result = get_optional_inputs(policy_check)
-        assert result == {"INPUT_IGNORE-FINDING",  "INPUT_ALLOW-DYNAMIC-REF-WITHOUT-VERSION",  "INPUT_EXCLUDE-RESOURCE-TYPES"}
-
-
-    # test_get_sub_command_case1: failure expected because required os.environ[]s are not set
+    # case 14: test_get_sub_command_ENVIRON_NOT_SET: failure expected because required os.environ[]s are not set
     @unittest.expectedFailure
-    def test_get_sub_command_case12(self):
+    def test_get_sub_command_ENVIRON_NOT_SET(self):
         required = {"INPUT_TEMPLATE-PATH",  "INPUT_REGION"}
         assertRaises(ValueError, get_sub_command, required, True)
 
-    def test_get_sub_command_case13(self):
+    # case 15: test_get_sub_command_MEET_REQUIRED_INPUTS: success as valid inputs are provided.
+    def test_get_sub_command_MEET_REQUIRED_INPUTS(self):
         tft = os.getcwd() + './tf_validate.json'
         os.environ['INPUT_TEMPLATE-PATH'] = tft
         os.environ['INPUT_REGION'] = 'us-west-2'
         required = {"INPUT_TEMPLATE-PATH",  "INPUT_REGION"}
         expected = ['--template-path', tft, '--region', 'us-west-2']
         flags = get_sub_command(required, True)
-        assert set(flags) == set(expected)
+        self.assertEqual(set(flags), set(expected))
 
-    # test_get_treat_findings_as_non_blocking_flag_case1: failure expected because os.environ[TREAT_FINDINGS_AS_NON_BLOCKING] is not set
+    # case 16: test_get_treat_findings_as_non_blocking_flag_ENVIRON_NOT_SET: failure expected because os.environ[TREAT_FINDINGS_AS_NON_BLOCKING] is not set
     @unittest.expectedFailure
-    def test_get_treat_findings_as_non_blocking_flag_case14(self):
+    def test_get_treat_findings_as_non_blocking_flag_ENVIRON_NOT_SET(self):
         policy_check = CHECK_NO_NEW_ACCESS
         assertRaises(ValueError, get_treat_findings_as_non_blocking_flag, policy_check)
         
-    def test_get_treat_findings_as_non_blocking_flag_case15(self):
+    # case 17: test_get_treat_findings_as_non_blocking_flag_VALIDATE_POLICY: pass
+    def test_get_treat_findings_as_non_blocking_flag_VALIDATE_POLICY(self):
         policy_check = VALIDATE_POLICY
         result = get_treat_findings_as_non_blocking_flag(policy_check)
-        assert result == "" 
+        self.assertEqual(result, "")
         
-    def test_get_treat_findings_as_non_blocking_flag_case16(self):
+    # case 18: test_get_treat_findings_as_non_blocking_flag_CHECK_ACCESS_NOT_GRANTED_True: pass, a string of flag returned
+    def test_get_treat_findings_as_non_blocking_flag_CHECK_ACCESS_NOT_GRANTED_True(self):
         policy_check = CHECK_ACCESS_NOT_GRANTED
         os.environ[TREAT_FINDINGS_AS_NON_BLOCKING] = 'True'
         result = get_treat_findings_as_non_blocking_flag(policy_check)
-        assert result == ['--treat-findings-as-non-blocking']
+        self.assertEqual(result, ['--treat-findings-as-non-blocking'])
 
-    def test_get_treat_findings_as_non_blocking_flag_case17(self):
+    # case 19: test_get_treat_findings_as_non_blocking_flag_CHECK_ACCESS_NOT_GRANTED_False: pass, an empty string returned
+    def test_get_treat_findings_as_non_blocking_flag_CHECK_ACCESS_NOT_GRANTED_False(self):
         policy_check = CHECK_ACCESS_NOT_GRANTED
         os.environ[TREAT_FINDINGS_AS_NON_BLOCKING] = 'False'
         result = get_treat_findings_as_non_blocking_flag(policy_check)
-        assert result == "" 
+        self.assertEqual(result, "")
 
-    def test_build_command_case18(self):
+    # case 20: test_build_command_VALIDATE_POLICY: pass with correct parameters.
+    def test_build_command_VALIDATE_POLICY(self):
         policy_check = VALIDATE_POLICY
         tft = os.getcwd() + './tf_validate.json'
         os.environ['INPUT_TEMPLATE-PATH'] = tft 
@@ -131,23 +119,35 @@ class TfpvTest(unittest.TestCase):
         required = {"INPUT_TEMPLATE-PATH",  "INPUT_REGION"}
         optional = {"INPUT_IGNORE-FINDING"}
         command = build_command(policy_check, required, optional)
-        # print("command {}".format(command))
         expected = set([CLI_POLICY_VALIDATOR, 'validate', '--template-path', tft, '--region', 'us-west-2', '--ignore-finding', 'PASS_ROLE_WITH_STAR_IN_RESOURCE'])
         command_set = set(command)
-        assert command_set == expected
+        self.assertEqual(command_set, expected)
 
-#     def test_execute_command_case19(self):
-#         tft = os.getcwd() + '/tf_validate.json'
-#         cfg = os.getcwd() + '/default.yaml'
-#         command = [CLI_POLICY_VALIDATOR, 'validate', '--config', cfg, '--template-path', tft, '--region', 'us-west-2', '--treat-finding-type-as-blocking', 'ERROR']
-#         result = execute_command(command)
-#         assert result.find("BlockingFindings") != -1
+    # case 21: test_execute_command_EXCEPTION: exception raised
+    @patch("subprocess.run", side_effect=Exception("invalid-input"))
+    def test_execute_command_EXCEPTION(self, mock_run):
+        with pytest.raises(Exception) as exc:
+            result = execute_command()
+            assertNotEqual(str(exc.value).find("invalid-input"), -1)
 
-    def test_set_output_case20(self):
+    # case 21: test_execute_command_VALIDATE: pass with mockrun
+    @patch("main.subprocess.run")
+    def test_execute_command_VALIDATE(self, mock_run):
+        command = [CLI_POLICY_VALIDATOR, 'validate']
+        expected = "pass"
+
+        completed_process = subprocess.CompletedProcess(args=['command', 'args'], returncode=0, stdout=expected, stderr=b'error')
+        mock_run.return_value = completed_process
+
+        result = execute_command(command)
+        self.assertEqual(result, expected)
+
+    # case 23: test_set_output: pass
+    def test_set_output_WRITE_TO_FILE(self):
         os.environ['GITHUB_OUTPUT'] = '/tmp/f1'
         val = '{"BlockingFindings": [],"NonBlockingFindings": []}'
         set_output(val)
         res = subprocess.run(["grep", "BlockingFindings", "/tmp/f1"], check=True, capture_output=True, encoding="utf-8").stdout
         subprocess.run(["rm", "/tmp/f1"])
-        assert res.find("BlockingFindings") != -1
+        self.assertNotEqual(res.find("BlockingFindings"), -1)
 
